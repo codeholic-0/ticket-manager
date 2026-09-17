4a138a76cf6eb9fbe1fa426d8e482b500126622d
Debankan Roy <userx4006@gmail.com>
2026-09-17 22:21:25 +0530

    feat: Added redisconfig for redis cache

---
+++ inventory-service/src/main/java/dev/zen/inventory/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package dev.zen.inventory.config;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> stringRedisTemplate(
+            RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        StringRedisSerializer serializer = new StringRedisSerializer(StandardCharsets.UTF_8);
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
